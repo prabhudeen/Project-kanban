fc4504725758ad50cb2b4187dafa04b526346cf8
PRABHUDEEN GAUTAM <prabhudeengautam@192.168.1.10>
2024-01-03 13:04:50 +0530

    Updates

---
+++ precache-manifest.d54494c7088a9db9e780fcc9dafb11ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e60e9d9ca79584200b38487b7e27a8c",
+    "url": "/Project-kanban/index.html"
+  },
+  {
+    "revision": "e9c4c000b435e8f633ad",
+    "url": "/Project-kanban/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9edcdc69a4667aa875a3",
+    "url": "/Project-kanban/static/js/2.890d1ceb.chunk.js"
+  },
+  {
+    "revision": "3f7c6a1bf2497f73c807",
+    "url": "/Project-kanban/static/js/3.c6fb0633.chunk.js"
+  },
+  {
+    "revision": "e9c4c000b435e8f633ad",
+    "url": "/Project-kanban/static/js/main.924b75c4.chunk.js"
+  },
+  {
+    "revision": "f110082b41c945fb5197",
+    "url": "/Project-kanban/static/js/runtime~main.2a747c06.js"
+  }
+]);
